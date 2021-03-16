85659ce36a408614b20f02ed376072783b916f6d
Daniel <danielbeaumont95@hotmail.co.uk>
2021-03-16 20:37:03 +0000

    added new user to db

--- db/data/development_data/users.js
+++ db/data/development_data/users.js
@@ -7,4 +7,10 @@ module.exports = [
     avatar_url:
       "https://media-exp1.licdn.com/dms/image/C5603AQEV7LURau5e5w/profile-displayphoto-shrink_200_200/0/1610970140095?e=1619049600&v=beta&t=9CaxUpCzsI-9rIcIBQV_xER-h1slJkXeTfL0oBA0DlA",
   },
+  {
+    username: "admin1",
+    password: "admin",
+    first_name: "admin",
+    surname: "adminthon",
+  },
 ];
